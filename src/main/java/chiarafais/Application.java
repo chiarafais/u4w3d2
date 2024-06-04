9bc6533e8741353fb91b358f83f4f82f141e7bd9
chiarafais <chiara0915@gmail.com>
2024-06-04 16:08:50 +0200

    add comment and fix

--- src/main/java/chiarafais/Application.java
+++ src/main/java/chiarafais/Application.java
@@ -20,6 +20,7 @@ public class Application {
         EventoDAO sd = new EventoDAO(em);
 
         //esercizio 1: cercare di salvare nel db i dati di un evento = OK
+        System.out.println("**************+*********ES 1***************************");
         Evento matrimonio = new Evento("matrimonio",LocalDate.of(2024, 9, 15),"questa è una descrizione", EventoType.PRIVATO, 500);
         Evento convention = new Evento("convention",LocalDate.of(2022, 10, 30),"questa è una descrizione", EventoType.PUBBLICO, 1000);
         Evento battesimo = new Evento("battesimo",LocalDate.of(2014, 8, 25),"questa è una descrizione", EventoType.PRIVATO, 700);
@@ -27,7 +28,7 @@ public class Application {
         Evento cresima = new Evento("cresima",LocalDate.of(2010, 7, 9),"questa è una descrizione", EventoType.PRIVATO, 200);
         Evento laurea = new Evento("laurea",LocalDate.of(2019, 3, 11),"questa è una descrizione", EventoType.PUBBLICO, 1500);
 
-        //commento causa vorrei evitare che si creino ogni volta li stessi eventi ma vorrei comunque tenere il codice!
+        //vorrei evitare che si creino ogni volta li stessi eventi ma vorrei comunque tenere il codice! (quindi commento tutti i sd.save())
 //        sd.save(matrimonio);
 //        sd.save(convention);
 //        sd.save(battesimo);
@@ -37,21 +38,21 @@ public class Application {
 
 
         //esercizio 2: getById = OK
+        System.out.println("**************+*********ES 2***************************");
         try {
             Evento provaGetById = sd.findById(4);
             System.out.println(provaGetById);
-
         } catch (NotFoundException ex) {
             System.out.println(ex.getMessage());
         }
         //Evento{id=4, titolo='battesimo', data_evento=2014-08-25, descrizione='questa è una descrizione', eventoType=PRIVATO, numero_max_partecipanti=700}
 
         //esercizio 3: delete = OK
+        System.out.println("**************+*********ES 3***************************");
         try {
-            sd.delete(6);
+            sd.delete(202);
         } catch (NotFoundException ex) {
             System.out.println(ex.getMessage());
-
         }
         //L'evento cresima è stato correttamente eliminato dal db!
 
